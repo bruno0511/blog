bf9a5101f2b301ef58abbf646e68fe02b5fe0398
bruno0511 <bruno.santiago13@hotmail.com>
2019-05-27 21:51:32 -0300

    farmacia2

--- atv2/resources/views/form-adicionar.blade.php
+++ atv2/resources/views/form-adicionar.blade.php
@@ -2,16 +2,16 @@
 
 @section('conteudo-principal')
 
-<h2> Adicionar Post</h2>
+<h2> Adicionar Medicamento</h2>
 
 <form method="post" action="{{url('/posts/adicionar')}}">
     @csrf
     <div class="form-group">
-        <label for="titulo">Titulo</label>
+        <label for="titulo">Nome</label>
         <input id="titulo" class="form-control" name="titulo" type="text">
     </div>
     <div class="form-group">
-        <label for="texto">Texto</label>
+        <label for="texto">Descrição</label>
         <textarea id="texto" class="form-control" name="texto" rows="3" placeholder="Diegite o texto"></textarea>
     </div>
 <button class= "btn btn-primary float-right mb-2" type="submit">Enviar</button>
